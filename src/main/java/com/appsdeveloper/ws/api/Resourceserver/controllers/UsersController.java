623e993280616cd8a8c092b6d1d35e0a2636bbc7
merved <mervenur.537@gmail.com>
2022-12-25 20:26:34 +0300

    postAuthorize

--- src/main/java/com/appsdeveloper/ws/api/Resourceserver/controllers/UsersController.java
+++ src/main/java/com/appsdeveloper/ws/api/Resourceserver/controllers/UsersController.java
@@ -1,10 +1,13 @@
 package com.appsdeveloper.ws.api.Resourceserver.controllers;
 
+import com.appsdeveloper.ws.api.Resourceserver.response.UserRest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,24 +22,20 @@ public class UsersController {
     @PreAuthorize("#id == #jwt.subject")
     @DeleteMapping(path = "/{id}")
     public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        SecurityContextHolder.getContext();
         return "Deleted user with id " + id + " and JWT subject " + jwt.getSubject();
     }
-
-
-
-
-/*
+    /*
     @PreAuthorize("hasAuthority('ROLE_developer') or #id == #jwt.subject")
     //@Secured("ROLE_developer")
     @DeleteMapping(path = "/{id}")
     public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
         return "Deleted user with id " + id + " and JWT subject " + jwt.getSubject();
-    }
-
+    }*/
 
     @PostAuthorize("returnObject.userId == #jwt.subject")
     @GetMapping(path = "/{id}")
     public UserRest getUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
-        return new UserRest("Sergey", "Kargopolov","5f3fb480-f86c-4514-8d23-ca88d66c6253");
-    }*/
+        return new UserRest("merve", "d","29be9f7f-2854-40ac-aafb-e5b0e2240154");
+    }
 }
--- src/main/java/com/appsdeveloper/ws/api/Resourceserver/security/WebSecurity.java
+++ src/main/java/com/appsdeveloper/ws/api/Resourceserver/security/WebSecurity.java
@@ -13,7 +13,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 
 //@Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled=true)
+@EnableGlobalMethodSecurity(securedEnabled=true, prePostEnabled=true)
 public class WebSecurity{
 	@Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
